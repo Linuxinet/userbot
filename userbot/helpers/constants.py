ebd0a8f2b82a8adcdd013ad7a56f515be7017a0f
Linuxinet <55936553+Linuxinet@users.noreply.github.com>
2020-12-21 21:27:29 +0530

    Update constants.py

--- userbot/helpers/constants.py
+++ userbot/helpers/constants.py
@@ -1,7 +1,7 @@
 class First:
     ALIVE = "`Baka! Anata wa watashi ga shinu to omotta?`"
-    REPO = "Click <a href=\"https://github.com/athphane/userbot\">here</a> to open Usebot's GitHub page."
-    CREATOR = "I was created by my master <a href=\"https://github.com/athphane\">Athphane</a> on a rainy day."
+    REPO = "Click <a href=\"https://github.com/Linuxinet/userbot/\">here</a> to open Usebot's GitHub page."
+    CREATOR = "I was created by my master <a href=\"https://github.com/Linuxinet\">Linuxinet</a> on a Boring day."
 
 
 class Weebify:
